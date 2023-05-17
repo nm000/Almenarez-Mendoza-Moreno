2982bbfa710b0f210848e711d8bfc5249fa3946c
MaycolMD <68664300+MaycolMD@users.noreply.github.com>
2023-05-17 15:21:38 -0500

    Update compresorp.py

--- compresorp.py
+++ compresorp.py
@@ -2,5 +2,5 @@ import sys
 from mainCompresorp import compress
 
 file_path_source = sys.argv[1]
-file_path_destination = 'comprimido.elmejorprofesor'
+file_path_destination = 'comprimidop.elmejorprofesor'
 compressTask = compress(file_path_source, file_path_destination)
