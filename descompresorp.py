847df1dd188f02f5bab7825cdbb2c31819cbc95f
MaycolMD <68664300+MaycolMD@users.noreply.github.com>
2023-05-17 15:22:03 -0500

    Update descompresorp.py

--- descompresorp.py
+++ descompresorp.py
@@ -2,5 +2,5 @@ import sys
 from mainDecompressorp import decompress
 
 file_path_source2 = sys.argv[1]
-file_path_destination2 = 'descomprimido-elmejorprofesor.txt'
+file_path_destination2 = 'descomprimidop-elmejorprofesor.txt'
 decompressTask = decompress(file_path_source2, file_path_destination2)
